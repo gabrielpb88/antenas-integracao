70cbdb8385750af86a77e71fede668d19ab1d7b1
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-03-05 01:28:33 -0300

    Criado ReuniaoRepository

---
+++ src/main/java/br/com/fatecsjc/repositories/ReuniaoRepository.java
@@ -0,0 +1,20 @@
+package br.com.fatecsjc.repositories;
+
+import br.com.fatecsjc.config.Database;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class ReuniaoRepository {
+
+    private MongoCollection<Document> collection;
+
+    public ReuniaoRepository(){
+        MongoDatabase db = Database.getConnection();
+        db.getCollection("reuniao");
+    }
+
+    public void salvar(Document reuniao) {
+        collection.insertOne(reuniao);
+    }
+}
