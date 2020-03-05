d93219a1d57ac7111a753a5c583e0b0a1bf10199
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-03-05 01:08:06 -0300

    Refatorado EmpresarioRepository

---
+++ src/main/java/br/com/fatecsjc/repositories/EmpresarioRepository.java
@@ -0,0 +1,35 @@
+package br.com.fatecsjc.repositories;
+
+import br.com.fatecsjc.config.Database;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+
+public class EmpresarioRepository {
+
+	private MongoCollection<Document> collection;
+
+	public EmpresarioRepository(){
+		MongoDatabase db = Database.getConnection();
+		collection = db.getCollection("empresario");
+	}
+
+	public void save(Document empresario) {
+		collection.insertOne(empresario);
+	}
+
+	public Document update(Document empresario) {
+		collection.replaceOne(Filters.eq("_id", empresario.get("_id")), empresario);
+		return findByEmail(empresario.get("email").toString());
+	}
+
+	public FindIterable<Document> findAll() {
+		return collection.find();
+	}
+
+	public Document findByEmail(String email) {
+		return collection.find(Filters.eq("email", email)).first();
+	}
+}
