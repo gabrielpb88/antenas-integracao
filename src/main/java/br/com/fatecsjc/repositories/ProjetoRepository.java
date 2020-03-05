8c864ea3dd397a806661ae9525d01fe623f50a86
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-03-05 01:49:49 -0300

    Adicionado busca por Cadi, por Aluno, BuscaSemCadi, BuscaSemAluno

--- src/main/java/br/com/fatecsjc/repositories/ProjetoRepository.java
+++ src/main/java/br/com/fatecsjc/repositories/ProjetoRepository.java
@@ -5,8 +5,10 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import com.mongodb.client.result.DeleteResult;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 public class ProjetoRepository {
 
@@ -29,6 +31,32 @@ public class ProjetoRepository {
 		return collection.find(Filters.eq("responsavel-empresario", email));
 	}
 
+	public FindIterable<Document> findByCadi(String email) {
+		return collection.find(Filters.eq("responsavel-cadi", email));
+	}
+
+	public FindIterable<Document> findByAluno(String email) {
+		return collection.find(Filters.eq("responsavel-aluno", email));
+	}
+
+	public FindIterable<Document> findWithoutCadi() {
+		return collection.find(Filters.eq("responsavel-cadi", ""));
+	}
+
+	public FindIterable<Document> findWithoutAluno() {
+		return collection.find(Filters.eq("responsavel-aluno", ""));
+	}
+
+	public Document atribuirAlunoResponsavel(String email, String id) {
+		collection.updateOne(Filters.eq("_id", new ObjectId(id)), Updates.set("responsavel-aluno", email));
+		return findById(id);
+	}
+
+	public Document atribuirProfessorResponsavel(String email, String id) {
+		collection.updateOne(Filters.eq("_id", new ObjectId(id)), Updates.set("responsavel-professor", email));
+		return findById(id);
+	}
+
 	public Document update(Document projeto) {
 		return collection.findOneAndUpdate(Filters.eq("_id", projeto.get("_id")), projeto);
 	}
@@ -41,7 +69,7 @@ public class ProjetoRepository {
 		return collection.find();
 	}
 
-	public Document findById(String _id) {
-		return collection.find(Filters.eq("_id", _id)).first();
+	public Document findById(String id) {
+		return collection.find(Filters.eq("_id", new ObjectId(id))).first();
 	}
 }
