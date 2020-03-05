cc2073a82f8fed8f2570ab0efeaf1d69cc44c607
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-03-05 01:48:20 -0300

    Refatorado AlunoRepository

---
+++ src/main/java/br/com/fatecsjc/repositories/AlunoRepository.java
@@ -0,0 +1,40 @@
+package br.com.fatecsjc.repositories;
+
+import br.com.fatecsjc.config.Database;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+
+public class AlunoRepository {
+
+    private MongoCollection<Document> collection;
+
+    public AlunoRepository() {
+        MongoDatabase db = Database.getConnection();
+        collection = db.getCollection("aluno");
+    }
+
+    public void save(Document aluno) {
+        collection.insertOne(aluno);
+    }
+
+    public Document login(String email, String senha) {
+        return collection.find(Filters.and(Filters.eq("email", email), Filters.eq("senha", senha))).first();
+    }
+
+    public Document update(Document aluno) {
+        collection.updateOne(Filters.eq("_id", aluno.get("_id").toString()), aluno);
+        return findByEmail(aluno.get("email").toString());
+    }
+
+    public Document findByEmail(String email) {
+        return collection.find(Filters.eq("email", email)).first();
+    }
+
+    public FindIterable<Document> findAll() {
+        return collection.find();
+    }
+
+}
