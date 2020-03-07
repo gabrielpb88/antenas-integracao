b69b23d48a85823648dbe3b55e16463b66b13bbe
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-03-07 19:46:24 -0300

    Criado metodo para conversao de objeto FileIterable para String

--- src/main/java/br/com/fatecsjc/utils/TextUtils.java
+++ src/main/java/br/com/fatecsjc/utils/TextUtils.java
@@ -1,4 +1,20 @@
 package br.com.fatecsjc.utils;
 
+import com.mongodb.client.FindIterable;
+import org.bson.Document;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 public class TextUtils {
+
+    /**
+     * Converte o iterable para uma String contendo todos os objetos
+     * @param iterable - Objeto a ser convertido para String
+     * @return String contendo todos os objetos
+     */
+    public static String converter(FindIterable<Document> iterable){
+        return StreamSupport.stream(iterable.spliterator(), false).map(Document::toJson)
+                .collect(Collectors.joining(", ", "[", "]"));
+    }
 }
