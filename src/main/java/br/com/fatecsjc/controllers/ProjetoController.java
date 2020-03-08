bd39e98819094de5e1fcafb011701df715dcb0af
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-03-08 01:08:58 -0300

    Adicionado dependencia slf4j-simple

--- src/main/java/br/com/fatecsjc/controllers/ProjetoController.java
+++ src/main/java/br/com/fatecsjc/controllers/ProjetoController.java
@@ -1,6 +1,7 @@
 package br.com.fatecsjc.controllers;
 
 import br.com.fatecsjc.services.ProjetoService;
+import br.com.fatecsjc.utils.TextUtils;
 import org.bson.Document;
 import org.json.JSONException;
 
@@ -12,66 +13,52 @@ import static spark.Spark.*;
 public class ProjetoController {
 
     private static ProjetoController controller;
-	private static ProjetoService service;
+    private static ProjetoService service;
 
-	private ProjetoController() {
-		service = new ProjetoService();
-	}
+    private ProjetoController() {
+        service = new ProjetoService();
+    }
 
     /**
      * Método estático encarregado de criar uma instancia do Controller e iniciar todos os Endpoints /projetos
      */
-	public static void init(){
-	    if(controller == null){
-	        controller = new ProjetoController();
+    public static void init() {
+        if (controller == null) {
+            controller = new ProjetoController();
         }
         initiateEndpoints();
     }
 
-	/**
-	 * Inicia todos os endpoints deste Controller
-	 */
-	private static void initiateEndpoints() {
+    /**
+     * Inicia todos os endpoints deste Controller
+     */
+    private static void initiateEndpoints() {
 
-		path("/projetos", () -> {
-			get("", (req, res) -> {
-				return StreamSupport.stream(service.findAll().spliterator(), false)
-						.map(Document::toJson)
-						.collect(Collectors.joining(", ", "[", "]"));
-			});
+        path("/projetos", () -> {
+            get("", (req, res) -> {
+                return TextUtils.converter(service.findAll());
+            });
 
             post("", (req, res) -> {
-                try {
-                    res.header("Access-Control-Allow-Origin", "*");
-                    String jsonString = req.body();
+                res.header("Access-Control-Allow-Origin", "*");
+                String jsonString = req.body();
 
-                    Document project = Document.parse(jsonString);
-                    service.save(project);
+                Document project = Document.parse(jsonString);
+                service.save(project);
 
-                    return project.toJson();
-                } catch (JSONException ex) {
-                    return "erro 500 " + ex;
-                }
+                return project.toJson();
             });
 
             put("", (req, res) -> {
-                try {
-                    res.header("Access-Control-Allow-Origin", "*");
-                    return service.update(Document.parse(req.body())) == null ? "projeto não encontrado"
-                            : "projeto deletado";
-                } catch (Exception ex) {
-                    throw ex;
-                }
+                res.header("Access-Control-Allow-Origin", "*");
+                return service.update(Document.parse(req.body())) == null ? "projeto não encontrado"
+                        : "projeto deletado";
             });
 
             delete("", (req, res) -> {
-                try {
-                    res.header("Access-Control-Allow-Origin", "*");
-                    return service.delete(Document.parse(res.body())).getDeletedCount() > 0;
-                } catch (Exception ex) {
-                    throw ex;
-                }
+                res.header("Access-Control-Allow-Origin", "*");
+                return service.delete(Document.parse(res.body())).getDeletedCount() > 0;
             });
-		});
-	}
+        });
+    }
 }
